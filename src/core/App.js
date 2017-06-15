c07b3b1c02133d589a5a7ad670e04229628292d5
u4bi <u4bi@u4bi.com>
2017-06-15 23:47:46 +0900

    delete tabs

--- src/core/App.js
+++ src/core/App.js
@@ -1,4 +1,2 @@
 export { U4biCard } from '../modules/U4biCard';
-export { ColorCard } from '../modules/ColorCard';
-
-export { Tabs } from '../modules/Tabs';
+export { ColorCard } from '../modules/ColorCard'; 
